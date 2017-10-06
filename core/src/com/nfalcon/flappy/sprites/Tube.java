ad0016ed0f23a253c56a7738523396645d099e48
falcon.nick@gmail.com <kitty793>
2017-10-06 13:32:37 -0500

    added tubes

--- core/src/com/nfalcon/flappy/sprites/Tube.java
+++ core/src/com/nfalcon/flappy/sprites/Tube.java
@@ -1,4 +1,41 @@
 package com.nfalcon.flappy.sprites;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
 public class Tube {
+    public static final int VARIABILITY = 130;
+    public static final int TUBE_GAP = 100;
+    public static final int MIN_OPENING = 120;
+
+    private Texture topTube, bottomTube;
+    private Vector2 posTopTube, posBottomTube;
+    private Random rnd;
+
+    public Tube(float x) {
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+        rnd = new Random();
+
+        posTopTube = new Vector2(x, rnd.nextInt(VARIABILITY) + TUBE_GAP + MIN_OPENING);
+        posBottomTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBottomTube() {
+        return posBottomTube;
+    }
 }
--- core/src/com/nfalcon/flappy/states/PlayState.java
+++ core/src/com/nfalcon/flappy/states/PlayState.java
@@ -5,14 +5,18 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.nfalcon.flappy.FlappyDemo;
 import com.nfalcon.flappy.sprites.Bird;
+import com.nfalcon.flappy.sprites.Tube;
 
 public class PlayState extends State {
     private Bird bird;
+    private Tube tube;
+
     private Texture bg;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Bird(50,FlappyDemo.HEIGHT);
+        bird = new Bird(50,FlappyDemo.HEIGHT-150);
+        tube = new Tube(100);
         bg = new Texture("bg.png");
         cam.setToOrtho(false, FlappyDemo.WIDTH/2, FlappyDemo.HEIGHT/2);
     }
@@ -42,6 +46,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBottomTube().x, tube.getPosBottomTube().y);
         sb.end();
     }
 }
